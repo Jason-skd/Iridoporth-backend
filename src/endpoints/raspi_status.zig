805d751f845eaea789a0e0b891726e755a5de223
Jason-skd <wintor76111@gmail.com>
2026-05-29 16:22:01 +0800

    refactor(raspi): make raspi_status endpoint file as struct

--- src/endpoints/raspi_status.zig
+++ src/endpoints/raspi_status.zig
@@ -5,39 +5,39 @@ const zap = @import("zap");
 
 const Context = @import("../context.zig");
 
-pub const RaspiStatusEndpoint = struct {
-    path: []const u8 = "/api/v1/raspi/status",
-    error_strategy: zap.Endpoint.ErrorStrategy = .log_to_console,
+pub const RaspiStatusEndpoint = @This();
 
-    pub fn get(_: *RaspiStatusEndpoint, arena: Allocator, ctx: *Context, r: zap.Request) !void {
-        const RaspiStatusResponse = struct { ok: bool = true, data: struct {
-            available: bool,
-            name: ?[]const u8 = null,
-            cpu_temperature: ?f32 = null,
-            cpu_usage: ?f32 = null,
-            memory_usage: ?f32 = null,
-        } };
+const RaspiStatusResponse = struct { ok: bool = true, data: struct {
+    available: bool,
+    name: ?[]const u8 = null,
+    cpu_temperature: ?f32 = null,
+    cpu_usage: ?f32 = null,
+    memory_usage: ?f32 = null,
+} };
 
-        r.setHeader("Content-Type", "application/json") catch {};
+path: []const u8 = "/api/v1/raspi/status",
+error_strategy: zap.Endpoint.ErrorStrategy = .log_to_console,
 
-        const response = switch (ctx.raspi) {
-            .unavailable => RaspiStatusResponse{
-                .data = .{
-                    .available = false,
-                },
+pub fn get(_: *RaspiStatusEndpoint, arena: Allocator, ctx: *Context, r: zap.Request) !void {
+    r.setHeader("Content-Type", "application/json") catch {};
+
+    const response = switch (ctx.raspi) {
+        .unavailable => RaspiStatusResponse{
+            .data = .{
+                .available = false,
             },
-            .status => |status| RaspiStatusResponse{
-                .data = .{
-                    .available = true,
-                    .name = status.name,
-                    .cpu_temperature = status.cpu_temperature.load(.monotonic),
-                    .cpu_usage = status.cpu_usage.load(.monotonic),
-                    .memory_usage = status.memory_usage.load(.monotonic),
-                },
+        },
+        .status => |status| RaspiStatusResponse{
+            .data = .{
+                .available = true,
+                .name = status.name,
+                .cpu_temperature = status.cpu_temperature.load(.monotonic),
+                .cpu_usage = status.cpu_usage.load(.monotonic),
+                .memory_usage = status.memory_usage.load(.monotonic),
             },
-        };
+        },
+    };
 
-        const body = try std.json.Stringify.valueAlloc(arena, response, .{});
-        try r.sendBody(body);
-    }
-};
+    const body = try std.json.Stringify.valueAlloc(arena, response, .{});
+    try r.sendBody(body);
+}
