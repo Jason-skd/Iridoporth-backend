a174eead7efe4dcbf4373ca6c5c3349689186b20
Jason-skd <wintor76111@gmail.com>
2026-06-17 19:25:46 +0800

    feat(user): add domain struct

---
+++ src/domain/UserSession.zig
@@ -0,0 +1,15 @@
+pub const UserSession = @This();
+
+id: i64,
+user_id: i64,
+method: Method,
+token_hash: []const u8,
+created_at: i64,
+expires_at: i64,
+last_used_at: i64,
+revoked_at: ?i64,
+
+pub const Method = enum {
+    anonymous_cookie,
+    password_login,
+};
